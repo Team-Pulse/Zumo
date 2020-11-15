17ac831361a0dfef5cbc3afb85cd77fcbbe54752
DESKTOP-0V4BRH4\roope <roberras@metropolia.fi>
2020-11-15 14:26:31 +0200

    Assignment 4 Task 1 // Kesken

---
+++ ZumoBot.cydsn/Roope.c
@@ -0,0 +1,81 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "Motor.h"
+#include "Ultra.h"
+#include "Nunchuk.h"
+#include "Reflectance.h"
+#include "Gyro.h"
+#include "Accel_magnet.h"
+#include "LSM303D.h"
+#include "IR.h"
+#include "Beep.h"
+#include "mqtt_sender.h"
+#include <time.h>
+#include <sys/time.h>
+#include "serial1.h"
+#include <unistd.h>
+
+#if 0
+void roopePrint(void)
+{
+    printf("Cool story, jees\n");
+    
+    while(true)
+    {
+        vTaskDelay(100); 
+    }
+}
+#endif
+
+#if 1
+//IR receiverm - how to wait for IR remote commands
+void zmain(void)
+{
+    IR_Start();
+    motor_start();
+    printf("\n\nIR test\n");
+    
+    IR_flush(); // clear IR receive buffer
+    printf("Buffer cleared\n");
+    
+    bool led = false;
+    // Toggle led when IR signal is received
+    while(true)
+    {
+        // toggle led state when button is pressed
+        if(SW1_Read() == 0) {
+            led = !led;
+            BatteryLed_Write(led);
+            if(led) printf("Led is ON\n");
+            else printf("Led is OFF\n");
+            Beep(1000, 150);
+            motor_forward(10, 500);
+            motor_forward(0, 0);
+            while(SW1_Read() == 0) vTaskDelay(10); // wait while button is being pressed
+        }  
+        IR_wait();  // wait for IR command
+        led = !led;
+        BatteryLed_Write(led);
+        if(led) printf("Led is ON\n");
+        else printf("Led is OFF\n");
+        
+        IR_isr_Enable();
+        motor_forward(10, 4000); 
+        motor_forward(0, 0);      
+    }    
+ }   
+#endif
+/* [] END OF FILE */
