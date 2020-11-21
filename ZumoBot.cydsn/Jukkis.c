f9a8f8cc9004d947566e4d40d7132ab797be178a
Jukkis <jukkajla@metropolia.fi>
2020-11-21 17:25:29 +0200

    no message

--- ZumoBot.cydsn/Jukkis.c
+++ ZumoBot.cydsn/Jukkis.c
@@ -99,11 +99,12 @@ void zmain(void)
         vTaskDelay(200);
     }
 }  
-
+#if 1
 void tank_turn(uint8 l_speed, uint8 r_speed, uint32 delay){
     SetMotors(0,1, l_speed, r_speed, delay);
     r_speed = 20;
 }
+#endif
 int random_number(int min, int max)  
                              
 { 
@@ -119,15 +120,19 @@ int random_number(int min, int max)
 
 #endif
 
-#if 1
+#if 0
+    
       void zmain(void)
-{
+    {
+
+    struct sensors_ dig;
+    _Bool black = 0;
+    int count = 0;
+    
     motor_start();
     IR_Start();
-    struct sensors_ dig;
-    
     reflectance_start();
-    reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
+    reflectance_set_threshold(14000, 9000, 11000, 11000, 9000, 14000); // set center sensor threshold to 11000 and others to 9000
     
     while(SW1_Read());
     BatteryLed_Write(1);
@@ -138,17 +143,140 @@ int random_number(int min, int max)
     
     reflectance_digital(&dig);
     
-    while(dig.L3 == 0 && dig.R3 == 0){
+        while(dig.L3 == 0 && dig.R3 == 0){
             
         reflectance_digital(&dig);
             
     }
-    motor_forward(0,0);
-    
-    while(true)
+    black = true;
+    while(black)
     {
-        vTaskDelay(100);
+        reflectance_digital(&dig);
+        black = dig.L3 || dig.R3;
     }
-}   
+    count++;
+    motor_forward(0,0);
+    printf("Count is: %d \n", count);
+    IR_wait();
+    motor_forward(10,0);
+    
+        while(true){
+        vTaskDelay(100);
+        }
+    }
+    
 
+#endif
+
+#if 0
+    
+      void zmain(void)
+    {
+
+    struct sensors_ dig;
+    _Bool black = 0;
+    int count = 0;
+    
+    motor_start();
+    IR_Start();
+    reflectance_start();
+    reflectance_set_threshold(14000, 9000, 11000, 11000, 9000, 14000); // set center sensor threshold to 11000 and others to 9000
+    
+    while(SW1_Read());
+    BatteryLed_Write(1);
+    vTaskDelay(1000);
+    BatteryLed_Write(0);
+    
+    motor_forward(100, 0);
+    
+    reflectance_digital(&dig);
+    
+        while(dig.L3 == 0 && dig.R3 == 0){
+            
+        reflectance_digital(&dig);
+            
+    }
+    black = true;
+    while(black)
+    {
+        reflectance_digital(&dig);
+        black = dig.L3 || dig.R3;
+    }
+
+        count++;
+        motor_forward(0,0);
+        printf("Count is: %d \n", count);
+        IR_wait();
+        
+        while(count < 5)
+        motor_forward(10,0);
+ 
+
+    
+        while(true){
+        vTaskDelay(100);
+        }
+    
+    }
+
+#endif
+
+#if 1 //Week3 Assignment 1
+    void zmain(void)
+    {
+    
+        struct sensors_ normal;
+        struct sensors_ sensors;
+        bool line = false;
+        bool done = false;
+        int count = 0;
+        
+        motor_start();
+        printf("WRUUUM!");
+        reflectance_start();
+        motor_forward(0,0);
+        IR_Start();
+        IR_flush();
+        
+        while(SW1_Read());
+        
+        vTaskDelay(1000);
+        
+        while(true)
+        {
+            reflectance_read(&normal);
+            reflectance_digital(&sensors);
+            
+            bool is_on_track = sensors.L1 && sensors.R1;
+            bool is_on_line = sensors.L3 && sensors.L2 && sensors.R2 && sensors.L2;
+            
+            printf("%d", count);
+            
+            if(is_on_track && !done)
+            {
+                motor_forward(10,0);
+            }
+            if(is_on_line)
+            {
+                line = true;
+            }
+            if(!is_on_line && line)
+            {
+                count++;
+                line = false;
+            }
+            if(count == 1)
+            {
+                motor_forward(0,0);
+                IR_wait();
+
+                motor_forward(0,0);
+                count++;
+            }
+
+           
+        }                   
+    }
+
+    
 #endif
